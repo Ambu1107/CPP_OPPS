9e4ebb0edede278a80581c37e28c6c9805367649
Ambu1107 <ambikapolashetawar@gmail.com>
2026-08-19 19:22:38 +0530

    Update and rename main.cpp to Lab_4.cpp

--- Lab_4.cpp
+++ Lab_4.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 
 class employee {
-	public:
+	public:    //Public Data Members
 		string name;
 		int id;
 		char dept;
 		float increment;
-	
+
+	// Default Constructor
 	employee() {
 		cout<<"Inside Constructor"<<endl;
 		increment=10;
 		cout<<"Salary: ";
 		cin>>salary;
 	}
-	
+
+	// Membor Function (To set the data)
 	void set_info() {
 		cout<<"Enter name: ";
 		cin>>name;
@@ -23,18 +25,19 @@ class employee {
 		cout<<"Enter dept: ";
 		cin>>dept;
 	}
-	
+
+    // Membor Function (To get the data)
 	void get_info() {
 		cout<<"----Employee Details----"<<endl;
 		cout<<"Name: "<<name<<endl<<"Employee ID: "<<id<<endl<<"Department: "<<dept<<endl<<"Salary: "<<salary<<endl<<"Increment: "<<increment<<"%"<<endl;
 		cout<<"------------------"<<endl;
 	}
 	
-	private:
+	private:    // Private Data Members
 		float salary;
 };
 
-employee e1,e2;
+employee e1,e2;  // Object Creation
 
 int main() {
 	e1.set_info();
