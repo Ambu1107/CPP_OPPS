38f52fb1b68e7d63edecd3c9e9f8a054dc664ce2
Ambu1107 <ambikapolashetawar@gmail.com>
2026-09-08 18:18:33 +0530

    Update Lab_7.cpp

--- Lab_7.cpp
+++ Lab_7.cpp
@@ -1,20 +1,23 @@
+// Function Overloading
+
 #include <iostream>
 using namespace std;
 
 class Employee {
-	public:
+	public:  // Access modifier (public) with data members
 		string name;
 		float salary;
 		char rating;
 		int exp;
 		
-		Employee() {
+		Employee() {  // Default Constructor
 			cout<<"Enter your name: "; cin>>name;
 			cout<<"Enter the year of experience you have: "; cin>>exp;
 			cout<<"Enter the rating: "; cin>>rating;
 			cout<<"Enter your salary: "; cin>>salary;
 		}
 		
+		// Member function to get the details
 		void get_info() {
 			cout<<"----Details----"<<endl;
 			cout<<"Name: "<<name<<endl;
@@ -34,13 +37,13 @@ class Employee {
 		float increment(float salary, int exp) {
 			float inc;
 			if (exp<5) {
-				inc = salary * 0.1;
+				inc = salary * 0.1;  // 10% increment
 			}
 			else if (exp<10) {
-				inc = salary * 0.2;
+				inc = salary * 0.2;  // 20% increment
 			}
 			else {
-				inc = salary * 0.3;
+				inc = salary * 0.3;  // 30% increment
 			}
 			return inc + salary;
 		}
@@ -49,31 +52,30 @@ class Employee {
 		float increment(float salary, int exp, char rating) {
 			float inc;
 			if (rating == 'A' || rating == 'B') {
-				inc = salary * 0.2;
+				inc = salary * 0.2;  // 20% increment
 			}
 			else if (rating == 'C' || rating == 'D') {
-				inc = salary * 0.1;
+				inc = salary * 0.1;  // 10% increment
 			}
 			else {
-				inc = salary * 0.05;
+				inc = salary * 0.05;  // 5% increment
 			}
 			return salary + inc;
 		}
 		
 };
 
-// Employee e1;
-//Employee e2;
-Employee e3;
+Employee e1, e2, e3;
+
 int main( ) {
-	//e1.get_info();
-	//cout<<"Your increment: "<<e1.increment(e1.salary)<<endl;
+	e1.get_info();
+	cout<<"Your Final Salary: "<<e1.increment(e1.salary)<<endl;
 	
-	//e2.get_info();
-	//cout<<"Your increment: "<<e2.increment(e2.salary, e2.exp)<<endl;
+	e2.get_info();
+	cout<<"Your Final Salary: "<<e2.increment(e2.salary, e2.exp)<<endl;
 	
 	e3.get_info();
-	cout<<"Your increment: "<<e3.increment(e3.salary, e3.exp, e3.rating)<<endl;
+	cout<<"Your Final Salary: "<<e3.increment(e3.salary, e3.exp, e3.rating)<<endl;
 	
 	return 0;
 }
